924bf41d3f7cdef6e654b6e5a3e071f701cfc2aa
shaid30044 <kaona30044@gmail.com>
2023-11-30 17:54:32 +0600

    set up review route and insert data on mongoDB

--- src/routes/review/index.js
+++ src/routes/review/index.js
@@ -5,12 +5,14 @@ const router = require("express").Router();
 
 router.get("/review", async (req, res) => {
   const result = await Review.find();
+
   res.send(result);
 });
 
 router.post("/review", verifyToken, async (req, res) => {
   const item = req.body;
   const result = await Review.create(item);
+
   res.send(result);
 });
 
@@ -18,6 +20,7 @@ router.delete("/review/:id", verifyToken, async (req, res) => {
   const id = req.params.id;
   const queryId = { _id: new ObjectId(id) };
   const result = await Review.deleteOne(queryId);
+
   res.send(result);
 });
 
