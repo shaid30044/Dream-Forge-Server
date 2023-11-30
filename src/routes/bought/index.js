55536030a46579d8227f2987bdc081c869daa820
shaid30044 <kaona30044@gmail.com>
2023-11-30 17:57:05 +0600

    set up bought route and insert data on mongoDB

--- src/routes/bought/index.js
+++ src/routes/bought/index.js
@@ -5,6 +5,7 @@ const router = require("express").Router();
 
 router.get("/bought", async (req, res) => {
   const result = await Bought.find();
+
   res.send(result);
 });
 
@@ -12,12 +13,14 @@ router.get("/bought/:id", async (req, res) => {
   const id = req.params.id;
   const query = { _id: new ObjectId(id) };
   const result = await Bought.findOne(query);
+
   res.send(result);
 });
 
 router.post("/bought", verifyToken, async (req, res) => {
   const item = req.body;
   const result = await Bought.create(item);
+
   res.send(result);
 });
 
@@ -31,6 +34,7 @@ router.patch("/bought/:id", verifyToken, async (req, res) => {
     },
   };
   const result = await Bought.updateOne(filter, updateDoc);
+
   res.send(result);
 });
 
